7365cacde7e7fa3151b5a1223550768f4d3ced94
Denis Zverkov <a@bc.com>
2018-02-26 09:45:52 +0900

    ppp

--- lab8.cpp
+++ lab8.cpp
@@ -24,7 +24,7 @@ DIR* dp = opendir(".");
             fcount++;
             opendir(res->d_name);
             dfs();
-            closedir(&(res->d_name));
+            closedir(info.);
     	}
     	else{
             size+= info.st_size;
